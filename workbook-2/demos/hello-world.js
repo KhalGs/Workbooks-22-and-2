f5bdc5e79f4e8a782bf957354f5d4b13a94b879d
Khaleel G <Kinghoodie221@gmail.comgit config --global init.defaultBranch main>
2024-05-23 09:17:40 -0400

    WorkBook 22 and 2

---
+++ workbook-2/demos/hello-world.js
@@ -0,0 +1,5 @@
+var firstName = "Aung";
+var lastName = "Aung";
+var cityName = "Yangon";
+var stateName = "Myanmar"
+console.log("Hello everyone, my name is " +  firstName + " " + lastName + ". " + "I was born and grew up in " + cityName + ", " + stateName + ".");
